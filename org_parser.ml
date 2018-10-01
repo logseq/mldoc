7e199bece45a5caa56bc0c8c1478180b8178a351
Tienson Qin <tiensonqin@gmail.com>
2018-10-01 21:57:13 +0800

    export to js.

--- org_parser.ml
+++ org_parser.ml
@@ -2,5 +2,16 @@ open Angstrom
 
 let parse input =
   match parse_string Heading.level input with
-  | Ok l -> print_int @@ List.length l
+  | Ok l -> List.length l
   | Error err -> failwith err
+
+let _ =
+  let open Js_of_ocaml in
+  Js.export "OrgParser"
+    (object%js
+       method parse input =
+         let str = Js.to_string input in
+         parse @@ str
+
+       method add x y = x + y
+    end)
