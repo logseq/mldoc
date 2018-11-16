04f9d0e18901b43b92205995b3876096a44f7f5f
Tienson Qin <tiensonqin@gmail.com>
2018-11-16 14:27:14 +0800

    change bench org file's path

--- bench/bench.ml
+++ bench/bench.ml
@@ -4,7 +4,7 @@ open Mldoc_org.Parser
 open Core
 open Core_bench.Std
 
-let text_org = load_file "/tmp/test.org"
+let text_org = load_file "./examples/syntax.org"
 
 let main () =
   Command.run (Bench.make_command [
