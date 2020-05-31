b8595beaeb571a68bc042146f58ab789211c30df
Tienson Qin <tiensonqin@gmail.com>
2020-05-31 15:09:16 +0800

    Fix markdown definition parsing

--- lib/syntax/markdown_definition.ml
+++ lib/syntax/markdown_definition.ml
@@ -9,23 +9,31 @@ open Type
  * : This is one definition of the second term.
  * : This is another definition of the second term. *)
 
+let term_definition =
+  let non_colon_or_eol = function
+      ':' | '\r' | '\n' -> false
+    | _ -> true in
+  let l = spaces *> satisfy non_colon_or_eol >>=
+    fun c ->
+    line <* (end_of_input <|> end_of_line)
+    >>| fun s ->
+    Char.escaped c ^ s
+  in
+  many1 l
+
+let definition_content_item =
+  spaces *> char ':' *> term_definition
+  >>| function lines ->
+    String.concat "\n" lines
+
 (* copy from verbatim *)
-let definition_content lines =
-  fix (fun definition_content ->
-      (spaces *> char ':' <* (optional spaces)) *> take_till is_eol <* optional eol
-      >>= fun line ->
-      lines := line :: !lines;
-      definition_content
-      <|>
-      (if !lines = [] then
-         fail "definition_content"
-       else
-         return (List.rev !lines)))
+let definition_content =
+  many1 definition_content_item
 
 let definition_parse config =
   let name = spaces *> line <* eol in
   name >>= fun name ->
-  definition_content (ref []) >>= fun lines ->
+  definition_content >>= fun lines ->
   let name = match parse_string (Inline.parse config) name with
     | Ok inlines -> inlines
     | Error _e -> [Inline.Plain name] in
