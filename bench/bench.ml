6be4ac43393e86d92746ba5d0ac5a400bea3fd9c
Tienson Qin <tiensonqin@gmail.com>
2020-06-26 07:34:57 +0800

    Add doc.org bench too

--- bench/bench.ml
+++ bench/bench.ml
@@ -4,17 +4,21 @@ open Mldoc.Conf
 open Core
 open Core_bench
 
-let text_org = load_file "./examples/syntax.org"
-let text_md = load_file "./examples/syntax.md"
+let doc_org = load_file "./examples/doc.org"
+let syntax_org = load_file "./examples/syntax.org"
+let syntax_md = load_file "./examples/syntax.md"
 
 let main () =
   Command.run (Bench.make_command [
-      Bench.Test.create ~name:"Mldoc Org mode parser" (fun () ->
+      Bench.Test.create ~name:"Mldoc Org mode parser (doc)" (fun () ->
           let config = {toc = true; heading_number = true; keep_line_break = false; format = "Org"; } in
-          ignore (parse config text_org));
+          ignore (parse config doc_org));
+      Bench.Test.create ~name:"Mldoc Org mode parser (syntax)" (fun () ->
+          let config = {toc = true; heading_number = true; keep_line_break = false; format = "Org"; } in
+          ignore (parse config syntax_org));
       Bench.Test.create ~name:"Mldoc Markdown parser" (fun () ->
           let config = {toc = true; heading_number = true; keep_line_break = false; format = "Markdown"; } in
-          ignore (parse config text_md));
+          ignore (parse config syntax_md));
     ])
 
 let () = main ()
