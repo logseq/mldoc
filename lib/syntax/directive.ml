00a30c26fc398f22e589007043e1707905fdf0a6
Tienson Qin <tiensonqin@gmail.com>
2020-07-29 07:29:33 +0800

    Fix directives parsing

--- lib/syntax/directive.ml
+++ lib/syntax/directive.ml
@@ -13,10 +13,19 @@ gives
 open Angstrom
 open Parsers
 open Type
+open Prelude
+
+let name =
+  (between_string "#+" ":" (take_while1 (fun c -> c <> ':' && non_eol c)))
+  >>= fun s ->
+  if starts_with s "BEGIN_" || starts_with s "begin_" then
+    fail "Directive might be a block"
+  else
+    return s
 
 let parse =
   let p =
     lift2 (fun name value -> [Directive (name, value)])
-      (between_string "#+" ":" (take_while1 (fun c -> c <> ':' && non_eol c)))
+      name
       (spaces *> optional_line) in
   between_eols p
