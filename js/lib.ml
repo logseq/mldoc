2d6516e4a650ce85a771045b0740035ad8509cd5
rcmerci <rcmerci@gmail.com>
2021-03-08 20:47:28 +0800

    export parseMarkdown method

--- js/lib.ml
+++ js/lib.ml
@@ -57,6 +57,23 @@ let _ =
            Js_of_ocaml.Js.string (Buffer.contents buffer)
          | Error error -> Js_of_ocaml.Js.string error
 
+       method parseMarkdown input config_json =
+         let str = Js.to_string input in
+         let config_json = Js.to_string config_json in
+         let buffer = Buffer.create 1024 in
+         let config_json = Yojson.Safe.from_string config_json in
+         match Conf.of_yojson config_json with
+         | Ok config ->
+           let ast = parse config str in
+           let document = Document.from_ast None ast in
+           let _ =
+             Sys_js.set_channel_flusher stdout (fun s ->
+                 Buffer.add_string buffer s)
+           in
+           generate "markdown" config document stdout;
+           Js_of_ocaml.Js.string (Buffer.contents buffer)
+         | Error error -> Js_of_ocaml.Js.string error
+
        method anchorLink s =
          let s = Js.to_string s in
          Js_of_ocaml.Js.string (Heading.anchor_link s)
