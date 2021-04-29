cb6092cd0d5c7686d12756c7946e65b1077163cf
rcmerci <rcmerci@gmail.com>
2021-04-29 15:57:54 +0800

    enhance(md): update md simplified-property-syntax

--- lib/syntax/markdown_property.ml
+++ lib/syntax/markdown_property.ml
@@ -8,12 +8,18 @@ open Type
  *)
 
 let property =
+  let only_key =
+    spaces *> take_while1 (fun c -> c <> ':' && non_space_eol c)
+    <* string "::" <* spaces
+    <* (eol *> return () <|> end_of_input)
+  in
   let key =
-    spaces *> take_while1 (fun c -> c <> ':' && non_space_eol c) <* string "::"
+    spaces *> take_while1 (fun c -> c <> ':' && non_space_eol c) <* string ":: "
   in
   let value =
     spaces *> take_till is_eol <* (eol *> return () <|> end_of_input)
   in
-  both key value
+  lift2 (fun k v -> (k, String.trim v)) key value
+  <|> (only_key >>| fun k -> (k, ""))
 
 let parse = many1 property >>| fun kvs -> Property_Drawer kvs
