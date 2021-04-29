45e6465e7306a2af581cfdaa0a4b7e1c1c081a84
rcmerci <rcmerci@gmail.com>
2021-04-29 11:35:14 +0800

    Revert "enhance(export): update level computation"
    This reverts commit ea829fbe2ab98d0db24895208a0117cdbf3644ab.
    
    'unordered' is new-introduced field in unordered-heading branch, not
    exists in master yet

--- lib/export/markdown.ml
+++ lib/export/markdown.ml
@@ -314,7 +314,7 @@ and heading_merely_have_embed { title; marker; priority; _ } =
   | _ -> None
 
 and heading refs state config h =
-  let { title; tags; marker; level; priority; unordered; _ } = h in
+  let { title; tags; marker; level; priority; _ } = h in
   let priority =
     match priority with
     | Some c -> "[#" ^ String.make 1 c ^ "]"
@@ -326,10 +326,8 @@ and heading refs state config h =
     | None -> ""
   in
   let top_heading_level = Option.default level state.top_heading_level in
-  let _ =
-    if Option.is_none state.top_heading_level then
-      state.top_heading_level <- Some level
-  in
+  if Option.is_none state.top_heading_level then
+    state.top_heading_level <- Some level;
   let level' =
     if state.embed_parent_indent_level > 0 && top_heading_level >= 2 then
       state.embed_parent_indent_level + level - top_heading_level + 1
@@ -340,10 +338,7 @@ and heading refs state config h =
   let f () =
     let heading_or_list =
       if config.heading_to_list then
-        if unordered then
-          [ Indent state.current_level; raw_text "-" ]
-        else
-          [ Indent (2 * state.current_level); raw_text "-" ]
+        [ Indent state.current_level; raw_text "-" ]
       else
         [ raw_text @@ String.make level' '#' ]
     in
@@ -850,7 +845,7 @@ let to_string config tl =
          | OneNewline -> "\n"
          | Indent n ->
            if config.heading_to_list then
-             String.make n ' '
+             String.make (2 * n) ' '
            else
              ""
          | RawText s -> s)
