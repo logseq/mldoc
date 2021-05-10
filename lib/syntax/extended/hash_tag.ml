02ce7735214743b759f683aaefef198efaf49426
rcmerci <rcmerci@gmail.com>
2021-05-10 10:56:51 +0800

    add '#' to hashtag delims & allow optional spaces before # heading

--- lib/syntax/extended/hash_tag.ml
+++ lib/syntax/extended/hash_tag.ml
@@ -6,13 +6,13 @@ let tag_delims = [ ','; ';'; '.'; '!'; '?'; '\''; '"'; ':' ]
 let hashtag_name =
   let hashtag_name_part =
     take_while1 (fun c ->
-        non_space_eol c && (not (List.mem c tag_delims)) && c <> '[')
+        non_space_eol c && (not (List.mem c tag_delims)) && c <> '[' && c <> '#')
     <|> page_ref
     (* ignore last consecutive periods *)
     <|> ( take_while (fun c -> List.mem c tag_delims)
           *> (satisfy is_space_eol *> return () <|> end_of_input)
           *> return `Finish
-        <|> (any_char_string >>| fun c -> `Continue c)
+        <|> (String.make 1 <$> not_one_of [ '#' ] >>| fun c -> `Continue c)
         >>= fun r ->
           match r with
           | `Finish -> fail "hashtag_name_part finish"
